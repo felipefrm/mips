14e673f86bf0fdca865bebe760aef27c5f54d0be
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-05-06 15:26:59 -0300

    Update questao1.asm

--- questao1.asm
+++ questao1.asm
@@ -27,30 +27,28 @@
 
  	li $s1, 1		#contador de termos
  	li $s2, 1		#denominador das divisões 
- 	li $s3, 4		#variavel fixa do numerador
- 	li $s4, 2		#variavel fixa para descobrir se o s1 é par ou impar 	
+ 	li $s3, 4		#constante do numerador
+ 	li $s4, 2		#constante para descobrir se o s1 é par ou impar 	
  loop: 	
  	slt $t0, $a0, $s1	# quantidade de termos < contador ?
  	bne $t0, $zero, return	# se 1, desvia para o retorno da função
 	rem $t0, $s1, $s4	# resto da divisão para verificar se o termo é par ou impar
- 	beq $t0, $zero, par	#se s1 for par, pula pra label par
  	mtc1 $s3,$f1		#converte inteiro para float e salva em f1
  	mtc1 $s2,$f2		#converte inteiro para float e salva em f2
- 	div.s $f1, $f1, $f2 	#divide a constante 4 por um numero impar
+ 	div.s $f1, $f1, $f2	#divide a constante 4 por um numero impar
+ 	beq $t0, $zero, par	#se s1 for par, pula pra label par
  	add.s $f12,$f12,$f1	#soma ao valor de pi o resultado da divisão ($f12 é o argumento syscall para impressão de float) 
- 	add $s1, $s1, 1		#incrementa 1 ao contador
+ 	j incremento
+ 	
+ par:
+ 	
+ 	sub.s $f12,$f12,$f1	#subtrai ao valor de pi o resultado da divisão
+
+  incremento:
+	add $s1, $s1, 1		#incrementa 1 ao contador
  	add $s2, $s2, 2		#incrementa 2 ao denominador das diviões (é sempre impar)
  	j loop
 
- par:
- 	mtc1 $s3,$f1		#converte inteiro para float e salva em f1
- 	mtc1 $s2,$f2		#converte inteiro para float e salva em f2
- 	div.s $f1, $f1, $f2	#divide a constante 4 por um numero impar	
- 	sub.s $f12,$f12,$f1	#subtrai ao valor de pi o resultado da divisão
- 	add $s1,$s1, 1		#incrmeenta 1 ao contador
- 	add $s2, $s2, 2		#incrementa 2 ao denominador das divisões (é sempre impar)
- 	j loop
- 
  return: 
  	jr $ra			#retorna o controle pra main
  	
