85a7e631a6570c1d261843ff38f802259d7d7d50
Felipe Melo <36086450+felipefrm@users.noreply.github.com>
2019-05-28 20:19:40 -0300

    Update questao1.asm

--- questao1.asm
+++ questao1.asm
@@ -1,9 +1,9 @@
 .data 
  
- 	msg1: .asciiz "\nDigite quantas termos para calcular o PI deseja: " 
- 	msg2: .asciiz "\nO valor de PI é: "
-.text
-.globl main
+ msg1: .asciiz "\nDigite quantas termos para calcular o PI deseja: " 
+ msg2: .asciiz "\nO valor de PI é: "
+ 	.text
+ 	.globl main
  	
  main:
  	li $v0, 4		#imprime a mensagem 1
@@ -25,12 +25,12 @@
  	
  CalculaPi:
  
-   	addi $sp, $sp, -16  #abre espaço na pilha para os registradores
+    	addi $sp, $sp, -16  #abre espaço na pilha para os registradores
     	sw $s1, 0($sp)
    	sw $s2, 4($sp)    #salva na pilha os registradores que serao utilizados
     	sw $s3, 8($sp)
     	sw $s4, 12($sp)
-	li $s1, 1		#contador de termos
+    	li $s1, 1		#contador de termos
     	li $s2, 1		#denominador das divisões 
     	li $s3, 4		#constante do numerador
     	li $s4, 2		#constante para descobrir se o s1 é par ou impar 	
@@ -38,9 +38,9 @@
  loop: 	
  	slt $t0, $a0, $s1	# quantidade de termos < contador ?
  	bne $t0, $zero, return	# se 1, desvia para o retorno da função
-	rem $t0, $s1, $s4	# resto da divisão para verificar se o termo é par ou impar
  	mtc1 $s3,$f1		#converte inteiro para float e salva em f1
  	mtc1 $s2,$f2		#converte inteiro para float e salva em f2
+	rem $t0, $s1, $s4	# resto da divisão para verificar se o termo é par ou impar
  	div.s $f1, $f1, $f2	#divide a constante 4 por um numero impar
  	beq $t0, $zero, par	#se s1 for par, pula pra label par
  	add.s $f12,$f12,$f1	#soma ao valor de pi o resultado da divisão ($f12 é o argumento syscall para impressão de float) 
@@ -50,7 +50,7 @@
  	
  	sub.s $f12,$f12,$f1	#subtrai ao valor de pi o resultado da divisão
 
-  incremento:
+ incremento:
 	add $s1, $s1, 1		#incrementa 1 ao contador
  	add $s2, $s2, 2		#incrementa 2 ao denominador das diviões (é sempre impar)
  	j loop
